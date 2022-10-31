84753484ef4931c686aae4a88ee34d2110bbb705
belen <belen@DESKTOP-QGBIKBC>
2022-10-31 04:06:08 -0300

    Clase Sistema y su test. (con su propio paquete, incluir acá mismo el buscador más adelante)

---
+++ src/sistema/Sistema.java
@@ -0,0 +1,48 @@
+package sistema;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import elementosDelSistema.Desafio;
+import elementosDelSistema.Muestra;
+import elementosDelSistema.Proyecto;
+import elementosDelSistema.Usuario;
+
+public class Sistema {
+	List<Desafio> desafios = new ArrayList<Desafio>();
+	List<Muestra> muestras = new ArrayList<Muestra>();
+	List<Usuario> usuarios = new ArrayList<Usuario>();
+	List<Proyecto> proyectos = new ArrayList<Proyecto>();
+	
+	public List<Desafio> getDesafios() {
+		return desafios;
+	}
+	
+	public List<Muestra> getMuestras() {
+		return muestras;
+	}
+	
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+	
+	public List<Proyecto> getProyectos() {
+		return proyectos;
+	}
+	
+	public void agregarDesafio(Desafio desafio) {
+		this.getDesafios().add(desafio);
+	}
+	
+	public void agregarMuestra(Muestra muestra) {
+		this.getMuestras().add(muestra);
+	}
+	
+	public void agregarUsuario(Usuario usuario) {
+		this.getUsuarios().add(usuario);
+	}
+	
+	public void agregarProyecto(Proyecto proyecto) {
+		this.getProyectos().add(proyecto);
+	}
+}
---
+++ test/sistemaTest/sistemaTest.java
@@ -0,0 +1,61 @@
+package sistemaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import accionesDeProyecto.RestriccionPorFecha;
+import accionesDeProyecto.RestriccionTemporal;
+import elementosDelSistema.Desafio;
+import elementosDelSistema.Muestra;
+import elementosDelSistema.PerfilUsuario;
+import elementosDelSistema.Proyecto;
+import elementosDelSistema.Usuario;
+import sistema.Sistema;
+
+class sistemaTest {
+	Muestra muestra;
+	Desafio desafio;
+	Proyecto proyecto;
+	Usuario usuario;
+	PerfilUsuario perfil;
+	RestriccionTemporal restriccion;
+	Sistema sistema;
+	
+	@BeforeEach
+	void setUp(){
+		restriccion = new RestriccionPorFecha(LocalDate.now());
+		desafio = new Desafio(1, 2, restriccion);
+		proyecto = new Proyecto("String1", "String2");
+		perfil = new PerfilUsuario();
+		usuario = new Usuario("Pedro", perfil);
+		muestra = new Muestra(usuario);
+		sistema = new Sistema();
+	}
+
+
+	@Test
+	void agregarDatos() {
+		//Inicia con 0 en todo.
+		assertEquals(0, sistema.getDesafios().size());
+		assertEquals(0, sistema.getMuestras().size());
+		assertEquals(0, sistema.getProyectos().size());
+		assertEquals(0, sistema.getUsuarios().size());
+		
+		//Cargo en el sistema un dato de cada uno
+		sistema.agregarDesafio(desafio);
+		sistema.agregarMuestra(muestra);
+		sistema.agregarProyecto(proyecto);
+		sistema.agregarUsuario(usuario);
+		
+		//Corroboro que se haya incrementado la cantidad en cada una de las listas.
+		assertEquals(1, sistema.getDesafios().size());
+		assertEquals(1, sistema.getMuestras().size());
+		assertEquals(1, sistema.getProyectos().size());
+		assertEquals(1, sistema.getUsuarios().size());
+	}
+
+}
