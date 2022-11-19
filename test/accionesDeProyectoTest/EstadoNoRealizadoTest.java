a46f2685bdf310c5c755f057ce401b0e445ea5b3
Kafuka <Kafuka@DESKTOP-SDKCRB0>
2022-11-18 21:28:31 -0300

    Merge branch 'main' of git@github.com:BelenAlumine/unq-po2-tpFinal.git

--- test/accionesDeProyectoTest/EstadoNoRealizadoTest.java
+++ test/accionesDeProyectoTest/EstadoNoRealizadoTest.java
@@ -33,44 +33,31 @@ class EstadoNoRealizadoTest {
 	RecomendacionDeDesafio recomendacion;
 	AreaGeografica areaGeografica;
 	
+		
 	@BeforeEach
 	void setup() {
-		Usuario usuario;
-		Desafio desafio;
-		Muestra muestra;
-		Muestra muestra0;
-		RestriccionTemporal restriccionPorFecha;
-		EstadoDelDesafio desafioNoRealizado;
-		EstadoDelDesafio desafioEnCurso;
-		Proyecto proyecto;
-		PerfilUsuario perfil;
-		RecomendacionDeDesafio recomendacion;
-		AreaGeografica areaGeografica;
-		
-		@BeforeEach
-		void setup() {
-			areaGeografica = new AreaGeografica(0.0, 0.0, 1);
-			perfil = new PerfilUsuario(5, 5, 5);
-			usuario = new Usuario("Juancito", perfil, recomendacion);
-			muestra = new Muestra(usuario, areaGeografica);
-			muestra0 = new Muestra(usuario, areaGeografica);
+		areaGeografica = new AreaGeografica(0.0, 0.0, 1);
+		perfil = new PerfilUsuario(5, 5, 5);
+		usuario = new Usuario("Juancito", perfil, recomendacion);
+		muestra = new Muestra(usuario, areaGeografica);
+		muestra0 = new Muestra(usuario, areaGeografica);
 
-			desafioNoRealizado = new EstadoNoRealizado();
-			desafioEnCurso = new EstadoEnCurso();
-			restriccionPorFecha = new RestriccionPorFecha(LocalDate.now(), LocalDate.now());
-			desafio = new Desafio(15, 3, 20, restriccionPorFecha, areaGeografica); //desafío de 15 muestras, dificultad 3
-			proyecto = new Proyecto("String1", "String2");
-		}
+		desafioNoRealizado = new EstadoNoRealizado();
+		desafioEnCurso = new EstadoEnCurso();
+		restriccionPorFecha = new RestriccionPorFecha(LocalDate.now(), LocalDate.now());
+		desafio = new Desafio(15, 3, 20, restriccionPorFecha, areaGeografica); //desafío de 15 muestras, dificultad 3
+		proyecto = new Proyecto("String1", "String2");
+	}
 
-		@Test
-		void test() {
-			assertTrue(desafio.getEstadoDelDesafio() instanceof EstadoNoRealizado);
-		}
+	@Test
+	void test() {
+		assertTrue(desafio.getEstadoDelDesafio() instanceof EstadoNoRealizado);
+	}
 		
-		@Test
-		void cambioDeEstado() {
-			//Acá hay que ver como el usuario acepta el desafio, y corroborar que al aceptar cambie de estado
-			desafioNoRealizado.cambiarDeEstado(desafio);
-			assertTrue(desafio.getEstadoDelDesafio() instanceof EstadoEnCurso);
-		}
+	@Test
+	void cambioDeEstado() {
+		//Acá hay que ver como el usuario acepta el desafio, y corroborar que al aceptar cambie de estado
+		desafioNoRealizado.cambiarDeEstado(desafio);
+		assertTrue(desafio.getEstadoDelDesafio() instanceof EstadoEnCurso);
+	}
 }
--- test/accionesGeneralesTest/RecomendacionDeDesafioTest.java
+++ test/accionesGeneralesTest/RecomendacionDeDesafioTest.java
@@ -2,6 +2,7 @@ package accionesGeneralesTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import accionesDeProyecto.RestriccionTemporal;
