718e6694e66cae684080be37d2debc0a0d7ab772
Kafuka <Kafuka@DESKTOP-SDKCRB0>
2022-11-18 21:50:11 -0300

    Actualización por Merge de main

--- src/elementosDelSistema/Usuario.java
+++ src/elementosDelSistema/Usuario.java
@@ -6,7 +6,6 @@ import accionesGenerales.RecomendacionDeDesafio;
 import accionesGenerales.TipoDeRecomendacion;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
